2d319176486986462ee85dbef2a231c09e79b7f0
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2017-11-08 15:28:49 +0700

    add sell_log page

---
+++ resources/views/sell_log/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.default')
+@section('content')
+<form method="post" action="/sell_log/create">
+  <div class="form-group">
+    <label class="col-form-label" for="formGroupExampleInput">Sell_log ID</label>
+    <input type="text" class="form-control" name="id" placeholder="ID">
+  </div>
+  <div class="form-group">
+    <label class="col-form-label" for="formGroupExampleInput">Stock ID</label>
+    <input type="text" class="form-control" name="SID" placeholder="stock id">
+  </div>
+  <div class="form-group">
+    <label class="col-form-label" for="formGroupExampleInput">Customer ID</label>
+    <input type="text" class="form-control" name="CusID" placeholder="product line id">
+  </div>
+  <div class="form-group">
+    <label class="col-form-label" for="formGroupExampleInput">Sell date</label>
+    <input type="int" class="form-control" name="date" placeholder="yyyy-mm-dd">
+  </div>
+  <div class="form-group">
+    <label class="col-form-label" for="formGroupExampleInput">Total payment</label>
+    <input type="int" class="form-control" name="payment">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@stop
