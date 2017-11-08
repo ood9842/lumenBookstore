13bec3135419062e9a3c8b7457115cb5eeafd51c
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2017-11-09 00:12:33 +0700

    fix

--- database/seeds/Data_Stock_keepers.php
+++ database/seeds/Data_Stock_keepers.php
@@ -13,17 +13,17 @@ class Data_Stock_keepers extends Seeder
     {
       DB::table('stock_keepers')->insert([
         'staff_id'=>'STb01s001',
-        'store_workON'=>'SK003lc50'
+        'stock_workON'=>'SK003lc50'
       ]);
 
       DB::table('stock_keepers')->insert([
         'staff_id'=>'STb02s002',
-        'store_workON'=>'SK002lc20'
+        'stock_workON'=>'SK002lc20'
       ]);
 
       DB::table('stock_keepers')->insert([
         'staff_id'=>'STb03s003',
-        'store_workON'=>'SK001lc11'
+        'stock_workON'=>'SK001lc11'
       ]);
     }
 }
