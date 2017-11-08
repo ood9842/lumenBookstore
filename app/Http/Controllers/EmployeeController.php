2a768e156cc75122c63c5a080ce89689f57804a4
chalitphol kuensuwan <chalitphol@gmail.com>
2017-11-08 21:15:16 +0700

    complete employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -37,29 +37,34 @@ class EmployeeController extends BaseController
 //CRUD
     public function create(Request $request)
     {
-      $employee=new Employees;//create row
-      $employee->staff_id = $request->input('staff_id');
-      $employee->store_workON = $request->input('store_workON');
-
-
-      $employee->save();//store
-
+      $staff_id = $request->input('staff_id');
+      $work_on = $request->input('store_workON');
+      $employee=Employees::insert(array('staff_id' => $staff_id, 'store_workON' => $work_on));
+      // $employee=new Employees;//create row
+      // $employee->staff_id = $request->input('staff_id');
+      // $employee->store_workON = $request->input('store_workON');
+      //
+      //
+      // $employee->save();//store
+      // var_dump($staff_id);
+      // var_dump($work_on);
       return redirect('/employees');
     }
 
     public function update(Request $request, $id,$id2)
     {
-      $employee=Employees::where('staff_id','=',$id)->where('store_workON','=',$id2)->first();
+      $work_on = $request->input('store_workON');
+      $employee=Employees::where('staff_id','=',$id)->where('store_workON','=',$id2)->update(array('store_workON'=>$work_on));;
 
-      $employee->store_workON = $request->input('store_workON');
+      // // $employee->store_workON = $request->input('store_workON');
       //
-      $employee->save();//store
-      $employee->push();//store
+      // $employee::where('staff_id','=',$id)->update(array('store_workON'=>$work_on));//store
+    //  $employee->push();//store
 
       // var_dump($employee);
-      var_dump($employee->store_workON);
-      var_dump($request->input('store_workON'));
-      // return redirect('/employees');
+    //var_dump($employee->store_workON);
+      var_dump($work_on);
+      return redirect('/employees');
     }
 
     public function delete($id,$id2)
