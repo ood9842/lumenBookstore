1ce69f51aeaa2f7a0fc811522d18b90d156ef9b8
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2017-11-09 13:45:55 +0700

    edit old value at edit form

--- resources/views/stock_keeper/edit.blade.php
+++ resources/views/stock_keeper/edit.blade.php
@@ -3,7 +3,7 @@
 <form method="post" action="/stock_keeper/update/{{$stock_keeper->staff_id}}/{{$stock_keeper->stock_workON}}">
   <div class="form-group">
     <label class="col-form-label" for="formGroupExampleInput">Stock work place id/label>
-    <input type="text" class="form-control" name="stock_workON" placeholder="stock ID">
+    <input type="text" class="form-control" name="stock_workON" placeholder="stock ID" value="{{$stock_keeper->stock_workON}}" required>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
   </form>
