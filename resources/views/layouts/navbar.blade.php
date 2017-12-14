aa16799865aad13fc37b3238a69994995b8db6b7
ASUS_kwpinthong <kw.pinthong@gmail.com>
2017-12-14 07:42:26 +0700

    fix navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,6 +5,22 @@
         <ul class="nav navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="/">Overview</a>
+                <a class="nav-link" href="/books" >Book</a>
+                <a class="nav-link" href="/book_in_stocks">Book In Stock</a>
+                <a class="nav-link" href="/customers" >Customers</a>
+                <a class="nav-link" href="/persons" >Persons</a>
+                <a class="nav-link" href="/staffs" >Staffs</a>
+                <a class="nav-link" href="/managers" >Managers</a>
+                <a class="nav-link" href="/employees" >Employees</a>
+                <a class="nav-link" href="/stock_keepers" >Stock Keepers</a>
+                <a class="nav-link" href="/stores" >Store</a>
+                <a class="nav-link" href="/stocks" >Stocks</a>
+                <a class="nav-link" href="/publishers" >Publishers</a>
+                <a class="nav-link" href="/product_lines" >Product Lines</a>
+                <a class="nav-link" href="/sell_details" >Sell Detail</a>
+                <a class="nav-link" href="/sell_logs" >Sell Logs</a>
+                <a class="nav-link" href="/supply_details" >Supply Detail</a>
+                <a class="nav-link" href="/supply_logs" >Supply Logs</a>
             </li>
         </ul>
     </div>
